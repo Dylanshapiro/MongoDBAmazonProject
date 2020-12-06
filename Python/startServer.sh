15d9083d6a4fc819cf61c6659f8c3f8a5121f205
Dylan Shapiro <shapiro1997@gmail.com>
2020-12-05 23:22:35 -0500

    name change

--- Python/startServer.sh
+++
@@ -1,9 +0,0 @@
-sudo service mongodb stop 
-
-sudo rm /var/lib/mongodb/mongod.lock 
-
-sudo mongod --repair --dbpath /var/lib/mongodb 
-
-sudo mongod --fork --logpath /var/lib/mongodb/mongodb.log --dbpath /var/lib/mongodb 
-
-sudo service mongodb start
